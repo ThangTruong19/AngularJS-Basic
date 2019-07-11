b2e1badc8c17e0966d7710e5bdae17c2e0633cab
ThangTruong <tuyenamai@gmail.com>
2019-07-11 20:04:52 +0700

    Dependency Injection & Interpolation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
         'use strict';
 
         angular.module('myApp', [])
-            .controller('myController', function($scope) {
+            .controller('myController', ['$scope', '$filter', function($scope, $filter) {
                 $scope.name = "";
                 $scope.totalValue = 0;
                 $scope.keyup = function() {
@@ -10,7 +10,21 @@
                         $scope.totalValue += $scope.name.charCodeAt(i);
                     }
                 }
-            });
+                $scope.blur = function() {
+                    var upperName = $filter('uppercase');
+                    $scope.name = upperName($scope.name);
+                }
+            }])
+            .controller('myController2', ['$scope', function($scope) {
+                $scope.state = "happy";
+                $scope.flip = function() {
+                    if ($scope.state == "happy") {
+                        $scope.state = "sad";
+                    } else {
+                        $scope.state = "happy";
+                    }
+                };
+            }]);
     }
 
 )();
