1dae3796f8955c68eecbf02a3944b10ce0110444
ThangTruong <tuyenamai@gmail.com>
2019-07-14 11:08:23 +0700

    Digest Cycle

--- app.js
+++ app.js
@@ -24,7 +24,46 @@
                         $scope.state = "happy";
                     }
                 };
-            }]);
+            }])
+            .controller('myController3', ['$scope', '$filter', 'appendFilter', 'changeTextFilter',
+                function($scope, $filter, appendFilter, changeTextFilter) {
+                    $scope.text = "This is just test infomation";
+                    $scope.text2 = ""
+                    var upper = $filter('uppercase');
+                    $scope.text = upper($scope.text);
+                    $scope.blurInput = function() {
+                        console.log("Enter blurInput");
+                        appendFilter($scope.text2);
+                    }
+
+                }
+            ])
+            .controller('myController4', ['$scope', function($scope) {
+                $scope.showWatcherCount = function() {
+                    console.log("Number of watcher: " + $scope.$$watchersCount);
+                }
+                $scope.watcher1 = 0;
+                $scope.watcher2 = 0;
+                $scope.incrementWatcher = function() {
+                    $scope.watcher1 += 1;
+                    $scope.watcher2 += 1;
+                    $scope.watcher3 = 1;
+                }
+            }])
+            .filter('append', appendString)
+            .filter('changeText', changeText);
+
+        function appendString() {
+            return function(input) {
+                return input + " - Created By Custom Filter";
+            }
+        }
+
+        function changeText() {
+            return function(input, original, replace) {
+                return input.replace(original, replace);
+            }
+        }
     }
 
 )();
